b0ce1c31f2b0b78f7ad2067d1606a5621f580393
John Manos <johnalexandermanos@gmail.com>
2022-09-08 20:24:54 -0400

    Add theme-changing functionality with button, and update code snippets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Renderer2  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { MatBottomSheet, MatBottomSheetRef } from '@angular/material';
+import { GlobalsService } from './globals.service';
 import { NavLink } from './NavLink';
 
 @Component({
@@ -7,6 +10,14 @@ import { NavLink } from './NavLink';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  theme:string = 'default';
+
+  constructor(private bottomSheet: MatBottomSheet) {}
+  
+  openBottomSheet(): void {
+    this.bottomSheet.open(ThemeBottomSheet);
+  }
 
   activeLink: NavLink = null;
 
@@ -20,4 +31,27 @@ export class AppComponent {
     {path: '/contact', label: 'Contact'},
   ];
 
+}
+
+@Component({
+  selector: 'theme-bottom-sheet',
+  templateUrl: 'theme-bottom-sheet.html',
+})
+export class ThemeBottomSheet {
+  
+  constructor(private bottomSheetRef: MatBottomSheetRef<ThemeBottomSheet>, private globalsService: GlobalsService, @Inject(DOCUMENT) private document: Document,
+  private renderer: Renderer2) {}
+
+  theme = "default";
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+
+  changeTheme(value: string): void {
+    this.renderer.removeClass(this.document.body, this.globalsService.theme);
+    this.globalsService.theme = value;
+    this.renderer.addClass(this.document.body, value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './/app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatTabsModule, MatExpansionModule, MatIconModule, MatIconRegistry, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatTabsModule, MatExpansionModule, MatIconModule, MatIconRegistry, MatCardModule, MatBottomSheetModule, MatRadioModule } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent, ThemeBottomSheet } from './app.component';
 import { BioComponent } from './bio/bio.component';
 import { CodeComponent } from './code/code.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { GlobalsService } from './globals.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ThemeBottomSheet,
     BioComponent,
     CodeComponent,
     ProjectsComponent,
@@ -25,19 +30,28 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatTabsModule,
     MatExpansionModule,
     MatButtonModule,
+    MatListModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatBottomSheetModule,
+    MatRadioModule
   ],
-  providers: [],
+  entryComponents: [AppComponent, ThemeBottomSheet],
+  providers: [GlobalsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
   // Register font-awesome to use with Angular Material's <mat-icon>
-  constructor(matIconRegistry: MatIconRegistry) {
+  constructor(matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+    matIconRegistry.addSvgIcon(
+      'color-palette',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/palette-icon.svg'));
   }
 }
